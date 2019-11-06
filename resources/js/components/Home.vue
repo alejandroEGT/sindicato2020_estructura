ff76f33d0c27d9b7b9a738cb146c26fe242a9e57
Sumbex <bmontecino.96@gmail.com>
2019-11-06 12:04:44 -0300

    intento de front

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,51 +1,67 @@
 <template>
-  <div class="row justify-content-center">
-      <q-input v-model="email" label="Standard" />
+  <div class="row justify-center">
+    <div class="col-6">
+      <q-card
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+        <q-card-section>
+          <div class="text-h6 justify-center">
+            <q-tabs v-model="tab" class="text-teal">
+              <q-tab label="Login" name="Login" />
+              <q-tab label="Registro" name="Registro" />
+            </q-tabs>
+          </div>
+        </q-card-section>
 
-      <q-input v-model="password" label="Standard" />
-
-      <q-btn color="primary" @click="login" icon="mail" label="On Left" />
+        <q-card-section>
+          <q-input v-model="email" label="Ingrese su correo" />
+          <q-separator />
+          <q-input v-model="password" label="Ingrese su contraseña" />
+          <q-separator />
+          <br />
+          <q-btn
+            class="justify-center"
+            color="primary"
+            @click="login"
+            icon="mail"
+            label="Ingresar"
+          />
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-  .md-card {
-    width: 320px;
-    margin: 4px;
-    display: inline-block;
-    vertical-align: top;
-  }
-</style>
-
 <script>
 export default {
-  name: 'Media',
-  data(){
-    return{
-      email:'',
-      password:'',
-      error:false
-    }
+  name: "Media",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: false
+    };
   },
 
-  methods:{
-     redirect_create_user(){
-      this.$router.push('create') 
+  methods: {
+    redirect_create_user() {
+      this.$router.push("create");
     },
-    login(){
-        var app = this
-        this.$auth.login({
-            params: {
-              email: app.email,
-              password: app.password
-            }, 
-            success: function () {},
-            error: function () {},
-            rememberMe: true,
-            redirect: '/index',
-            fetchUser: true,
-        });       
-    },
+    login() {
+      var app = this;
+      this.$auth.login({
+        params: {
+          email: app.email,
+          password: app.password
+        },
+        success: function() {},
+        error: function() {},
+        rememberMe: true,
+        redirect: "/index",
+        fetchUser: true
+      });
+    }
   }
-}
+};
 </script>
